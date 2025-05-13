7dbc7c3779ada02f3d35297472f016663a1b1338
chomnr <mail@zeljko.me>
2025-05-12 21:16:33 -0400

    example

--- dev/App.tsx
+++ dev/App.tsx
@@ -5,6 +5,13 @@ import { CircularFlag, CircularFlagLanguage } from 'src'
 const App: Component = () => {
   return (
     <div class={styles.App}>
+      <CircularFlag countryCode="bq" width={100} height={100} />
+      <CircularFlag
+        countryCode="as"
+        width={100}
+        height={100}
+        cdn="https://hatscripts.github.io/circle-flags/flags/"
+      />
       <CircularFlag countryCode="es-ar" width={100} height={100} />
       <CircularFlagLanguage languageCode="an" width={100} height={100} />
     </div>
